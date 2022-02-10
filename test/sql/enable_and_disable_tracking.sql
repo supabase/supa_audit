6d8ac4e2c99aa8c796029ea52f9084ab2dd033f1
Oliver Rice <github@oliverrice.com>
2022-02-10 09:44:18 -0600

    stabalize tests be remapping columns dependent on oids

--- test/sql/enable_and_disable_tracking.sql
+++ test/sql/enable_and_disable_tracking.sql
@@ -24,15 +24,28 @@ begin;
 
 
     -- Only record with id = 2 should be present
+    with remap as (
+        select distinct on (u.id)
+            u.id,
+            row_number() over () stable_id
+        from
+            audit.record_version arv,
+            unnest(array[arv.record_id, arv.old_record_id]) u(id)
+        order by
+            u.id asc
+    )
     select
-        id,
-        record_id,
-        old_record_id,
+        arv.id,
+        r.stable_id as remapped_record_id,
+        ro.stable_id as remapped_old_record_id,
         op,
         table_schema,
         table_name,
         record
     from
-        audit.record_version;
-
+        audit.record_version arv
+        left join remap r
+            on arv.record_id = r.id
+        left join remap ro
+            on arv.old_record_id = ro.id;
 rollback;
