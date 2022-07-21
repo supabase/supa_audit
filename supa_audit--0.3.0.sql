f70600e43bea1fa060b7c93c3475508f8a462ec1
Philipp Defner <mail@notmyhostna.me>
2022-07-22 00:26:52 +0200

    Rename, for better review diff

--- supa_audit--0.3.0.sql
+++ supa_audit--0.3.0.sql
@@ -272,12 +272,80 @@ declare
         $1
     );
 
+    statement_row_i_d text = format(
+        'drop trigger if exists audit_i_d on %s;',
+        $1
+    );
+    
+    statement_row_u text = format(
+        'drop trigger if exists audit_u on %s;',
+        $1
+    );
+
     statement_stmt text = format(
         'drop trigger if exists audit_t on %s;',
         $1
     );
 begin
     execute statement_row;
+    execute statement_row_i_d;
+    execute statement_row_u;
     execute statement_stmt;
+end;
+$$;
+
+-- enable_selective_tracking accepts a relation and a list of columns which will be tracked.
+create or replace function audit.enable_selective_tracking(regclass, tracked_columns text[])
+    returns void
+    volatile
+    security definer
+    set search_path = ''
+    language plpgsql
+as $$
+declare
+    statement_row_i_d text = format('
+        create trigger audit_i_d
+            after insert or delete
+            on %s
+            for each row
+            execute procedure audit.insert_update_delete_trigger();',
+        $1
+    );
+    
+    statement_row_u text = format('
+        create trigger audit_u
+            after update of %s
+            on %s
+            for each row
+            execute procedure audit.insert_update_delete_trigger();',
+        array_to_string($2, ', '), $1
+    );
+
+    statement_stmt text = format('
+        create trigger audit_t
+            after truncate
+            on %s
+            for each statement
+            execute procedure audit.truncate_trigger();',
+        $1
+    );
+
+    pkey_cols text[] = audit.primary_key_columns($1);
+begin
+    if pkey_cols = array[]::text[] then
+        raise exception 'Table % can not be audited because it has no primary key', $1;
+    end if;
+
+    if not exists(select 1 from pg_trigger where tgrelid = $1 and tgname = 'audit_i_d') then
+        execute statement_row_i_d;
+    end if;
+    
+    if not exists(select 1 from pg_trigger where tgrelid = $1 and tgname = 'audit_u') then
+        execute statement_row_u;
+    end if;
+
+    if not exists(select 1 from pg_trigger where tgrelid = $1 and tgname = 'audit_t') then
+        execute statement_stmt;
+    end if;
 end;
 $$;
